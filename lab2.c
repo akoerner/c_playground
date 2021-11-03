8c1c570ba9ef89c8082a4a4fa10f0d2b658f5eef
Andrew Koerner <andrew.koerner@dlr.de>
2021-11-03 13:52:33 +0100

    updated lab #2

---
+++ lab2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    //Lab#2
+
+    printf("Hello, lab#23!\n");
+    printf("THIS SHOULD NOT PRINT\n");
+    
+    int i = 1;
+    
+    for(i = 1; i <= 7; i++){
+        printf("i: %i\n", i);
+    }
+
+}
