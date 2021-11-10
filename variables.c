b7934a03766e5cbcf1fec31b3a1ea47d0defe39e
Andrew Koerner <andrew.koerner@dlr.de>
2021-11-10 21:43:06 +0100

    added variables.c for lab 4

--- variables.c
+++ variables.c
@@ -2,18 +2,9 @@
 
 void main(){
 
-    int my_int_a;
-    float my_float_a;
-    char my_char = 'c';
-    char * my_char_array = "Hello, World!";
-    int my_int_b = 0;
+  #define or declare 3 variables of type int, float and char
 
-    my_int_a = 1337;
-    my_float_a = 1.2;
 
-    printf("my_int_a: %d \n", my_int_a);
-    printf("my_float_a: %f \n", my_float_a);
-    printf("my_char: %c \n", my_char);
-    printf("my_char_array: %s \n", my_char_array);
-    printf("my_int_b: %i \n", my_int_b);
+  
+
 }
