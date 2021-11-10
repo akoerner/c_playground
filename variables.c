1eb043d8ffde7a6a7ad73b725a57ae06072c6f66
Andrew Koerner <andrew.koerner@dlr.de>
2021-11-10 22:02:18 +0100

    added variables.c for lab 4

--- variables.c
+++ variables.c
@@ -2,9 +2,13 @@
 
 void main(){
 
-  #define or declare 3 variables of type int, float and char
+  //define or declare 3 variables of type int, float and char
 
 
-  
+
+  //assign any appropriate and legal/permitted value to each variable you defined  
+
+
+
 
 }
