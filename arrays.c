63d99c0ad56d9c7f9dd83950fa0c3d5db3680993
Andrew Koerner <andrew.koerner@dlr.de>
2021-12-08 20:39:36 +0100

    setup for lecture 7 and lab 6

---
+++ arrays.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+
+  float exam_scores[3] = {4.2, 3.2, 5.0};
+
+  printf("exam_scores[0]: %f \n", exam_scores[0]);
+  printf("exam_scores[1]: %f \n", exam_scores[1]);
+  printf("exam_scores[2]: %f \n", exam_scores[2]);
+}
---
+++ increment_decrement.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+    int my_int = 0;
+
+    my_int = my_int + 1;
+    printf("my_int: %d\n", my_int);
+    my_int++;
+    printf("my_int: %d\n", my_int);
+    my_int--;
+    printf("my_int: %d\n", my_int);
+    return 0;
+}
