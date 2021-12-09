d3fb23fcbfb24dc2e7b931ce557e18c6042186e8
Andrew Koerner <andrew.koerner@dlr.de>
2021-12-09 10:25:41 +0100

    updated lab6.c

--- lab6.c
+++ lab6.c
@@ -3,6 +3,7 @@ int main() {
 
     int numbers[25] = {55, 52, 13, 41, 94, 93, 2, 63, 57, 70, 59, 14, 77, 92, 42, 4, 81, 97, 27, 15, 44, 18, 53, 64, 90}; 
 
+    //print all the values in the "numbers" array using a for or while loop 
 
     return 0;
 }
